53d2a7c2b6724c91870fffde5df9027c33183c6d
anduser <https://github.com/natatallia254/Andersen_Java_Homework>
2022-05-18 15:41:01 +0300

    Lesson_2

---
+++ HW/src/main/java/Lesson_2/HomeWorkTask_2.java
@@ -0,0 +1,16 @@
+package Lesson_2;
+
+public class HomeWorkTask_2 {
+
+    public static void main(String[] args) {
+        Task_2(0);
+    }
+
+    public static void Task_2(int a) {
+        if (a >= 0) {
+            System.out.println("Число a=" + a + " положительное");
+        } else {
+            System.out.println("Число a=" + a + " отрицательное");
+        }
+    }
+}
