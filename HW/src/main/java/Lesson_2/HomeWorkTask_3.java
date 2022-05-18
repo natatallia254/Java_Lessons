7ef4e4873cc798919fdb3731a94edb9df5356b7f
anduser <https://github.com/natatallia254/Andersen_Java_Homework>
2022-05-18 15:45:18 +0300

    Lesson_2

---
+++ HW/src/main/java/Lesson_2/HomeWorkTask_3.java
@@ -0,0 +1,17 @@
+package Lesson_2;
+
+public class HomeWorkTask_3 {
+
+    public static void main(String[] args) {
+        Task_3(10);
+    }
+
+    public static boolean Task_3(int q) {
+        if (q >= 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
