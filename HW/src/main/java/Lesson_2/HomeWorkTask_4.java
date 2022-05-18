9c3b295e361ecd4948731cc4b47d705d906394a8
anduser <https://github.com/natatallia254/Andersen_Java_Homework>
2022-05-18 15:46:49 +0300

    Lesson_2

---
+++ HW/src/main/java/Lesson_2/HomeWorkTask_4.java
@@ -0,0 +1,13 @@
+package Lesson_2;
+
+public class HomeWorkTask_4 {
+    public static void main(String[] args) {
+        Task_4("Приветулечки тебе!", 6);
+    }
+
+    public static void Task_4(String txtToPrint, int s) {
+        for (int i = 0; i < s; i++) {
+            System.out.println(txtToPrint);
+        }
+    }
+}
